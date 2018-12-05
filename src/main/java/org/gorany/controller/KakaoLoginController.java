0128f39b76c230e03862e2ec644ca8bc5582d61f
salem <chu1070y@naver.com>
2018-12-05 16:28:00 +0900

    login

--- src/main/java/org/gorany/controller/KakaoLoginController.java
+++ src/main/java/org/gorany/controller/KakaoLoginController.java
@@ -5,9 +5,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.gorany.domain.UserVO;
+import org.gorany.service.GoogleLogin;
 import org.gorany.service.KakaoLogin;
 import org.gorany.service.LoginService;
+import org.gorany.service.NaverLogin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,4 +50,49 @@ public class KakaoLoginController {
 	  response.sendRedirect("/home");
 	  return "redirect:/home";
 	}
+	
+	@RequestMapping(value = "/naverlogin", produces = "application/json", method = { RequestMethod.GET,
+			RequestMethod.POST })
+	public String naverLogin(Model model, @RequestParam("code") String code, @RequestParam("state") String state,
+			HttpSession session) {
+
+		JsonNode token = NaverLogin.getAccessToken(code);
+		log.info("=====" + code);
+		log.info("=====" + state);
+		log.info("=====" + token);
+
+		JsonNode userInfo = NaverLogin.getNaverUserInfo(token.path("access_token").toString());
+
+		UserVO vo = NaverLogin.changeData(userInfo);
+
+//		vo.setSns_id("n" + vo.getSns_id());
+
+		session.setAttribute("login", vo);
+		model.addAttribute("login", vo);
+
+		return "login/naverLogin";
+	}
+	
+	@RequestMapping(value = "/googlelogin", produces = "application/json", method = { RequestMethod.GET,
+			RequestMethod.POST })
+	public String googleLogin(@RequestParam("code") String code, HttpServletRequest request,
+			HttpServletResponse response, HttpSession session, Model model) {
+
+		JsonNode token = GoogleLogin.getAccessToken(code);
+		log.info("=====" + code);
+		log.info("=====" + token);
+
+		JsonNode userInfo = GoogleLogin.getGoogleUserInfo(token.path("access_token").asText());
+
+		UserVO vo = GoogleLogin.changeData(userInfo);
+
+//		vo.setUser_snsId("g" + vo.getUser_snsId());
+
+		session.setAttribute("login", vo);
+		model.addAttribute("login", vo);
+
+		return "login/googleLogin";
+	}
+	
+
 }
--- src/main/java/org/gorany/service/NaverLogin.java
+++ src/main/java/org/gorany/service/NaverLogin.java
@@ -1,4 +1,5 @@
 package org.gorany.service;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -17,15 +18,22 @@ import org.gorany.domain.UserVO;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class KakaoLogin {
+import lombok.extern.java.Log;
+
+@Log
+public class NaverLogin {
+	
 	public static JsonNode getAccessToken(String autorize_code) {
-		final String RequestUrl = "https://kauth.kakao.com/oauth/token";
+
+		final String RequestUrl = "https://nid.naver.com/oauth2.0/token";
 
 		final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
 		postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
-		postParams.add(new BasicNameValuePair("client_id", "8392c8baf0d0c5aa5ba40677ff1e40e2")); // REST API KEY
-		postParams.add(new BasicNameValuePair("redirect_uri", "http://localhost:8080/kakaologin")); // 리다이렉트 URI
+		postParams.add(new BasicNameValuePair("client_id", "LDp9BYkvyaslYqUOz1h5")); // REST API KEY
+		postParams.add(new BasicNameValuePair("client_secret", "RDKyqh1axY"));
+		postParams.add(new BasicNameValuePair("redirect_uri", "http://localhost:8080/naverlogin")); // 리다이렉트 URI
 		postParams.add(new BasicNameValuePair("code", autorize_code)); // 로그인 과정중 얻은 code 값
+		postParams.add(new BasicNameValuePair("state", ""));
 
 		final HttpClient client = HttpClientBuilder.create().build();
 		final HttpPost post = new HttpPost(RequestUrl);
@@ -36,17 +44,14 @@ public class KakaoLogin {
 			final HttpResponse response = client.execute(post);
 			final int responseCode = response.getStatusLine().getStatusCode();
 
-			System.out.println("\nSending 'POST' request to URL : " + RequestUrl);
+			System.out.println("Sending 'POST' request to URL : " + RequestUrl);
 			System.out.println("Post parameters : " + postParams);
 			System.out.println("Response Code : " + responseCode);
 
 			// JSON 형태 반환값 처리
 			ObjectMapper mapper = new ObjectMapper();
 			returnNode = mapper.readTree(response.getEntity().getContent());
-			
-			System.out.println("response: " + response);
-			System.out.println("returnNode: " + returnNode);
-			
+
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (ClientProtocolException e) {
@@ -56,36 +61,34 @@ public class KakaoLogin {
 		} finally {
 			// clear resources
 		}
-
 		return returnNode;
 
 	}
+	
+	public static JsonNode getNaverUserInfo(String autorize_code) {
 
-	public static JsonNode getKakaoUserInfo(String autorize_code) {
-
-		final String RequestUrl = "https://kapi.kakao.com/v2/user/me";
+		final String RequestUrl = "https://openapi.naver.com/v1/nid/me";
 
 		final HttpClient client = HttpClientBuilder.create().build();
 		final HttpPost post = new HttpPost(RequestUrl);
 
 		// add header
 		post.addHeader("Authorization", "Bearer " + autorize_code);
-		System.out.println("autorize_code: " + autorize_code);
-		System.out.println("post: " + post);
+
 		JsonNode returnNode = null;
 
 		try {
 			final HttpResponse response = client.execute(post);
-			System.out.println("response: " + response);
 			final int responseCode = response.getStatusLine().getStatusCode();
 
 			System.out.println("\nSending 'POST' request to URL : " + RequestUrl);
 			System.out.println("Response Code : " + responseCode);
 
 			// JSON 형태 반환값 처리
+
 			ObjectMapper mapper = new ObjectMapper();
 			returnNode = mapper.readTree(response.getEntity().getContent());
-			System.out.println("returnNode: " + returnNode );
+
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (ClientProtocolException e) {
@@ -97,31 +100,28 @@ public class KakaoLogin {
 		}
 		return returnNode;
 	}
-
+	
 	public static UserVO changeData(JsonNode userInfo) {
+		
+		log.info("service -> userinfo : "+ userInfo);
+		
 		UserVO vo = new UserVO();
-		System.out.println("userInfo: " + userInfo);
-		vo.setStatus('O');
-		vo.setSns_id(userInfo.path("id").asText()); // id -> vo 넣기
-//		if (userInfo.path("kaccount_email_verified").asText().equals("true")) { // 이메일 받기 허용 한 경우
-//			vo.setUser_email(userInfo.path("kaccount_email").asText()); // email -> vo 넣기
-
-		JsonNode kakao_account = userInfo.path("kakao_account");
-		System.out.println("kakao_account는?: " + kakao_account);
 		
-		if (kakao_account.path("is_email_verified").asText().equals("true")) { // 이메일 받기 허용 한 경우
-			vo.setEmail(kakao_account.path("email").asText()); // email -> vo 넣기
-
-		} else { // 이메일 거부 할 경우 코드 추후 개발
-
-		}
-
-		JsonNode properties = userInfo.path("properties"); // 추가정보 받아오기
-		if (properties.has("nickname")) {
-			vo.setNickname(properties.path("nickname").asText());
+		JsonNode properties = userInfo.path("response");
+		log.info("properties : " + properties);
+		
+		vo.setSns_id(properties.path("id").asText());
+		
+		if(properties.has("nickname")) {
+			vo.setName(properties.path("nickname").asText());
+			vo.setAge(properties.path("age").asText());
+			vo.setSex(properties.path("gender").asText());
+			vo.setEmail(properties.path("email").asText());
 		}
 		
 		
+		log.info("vo : " + vo);
 		return vo;
 	}
+
 }
